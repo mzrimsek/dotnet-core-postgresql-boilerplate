7420faa817bc42eadb306a4cce1834b2152ecfd5
Mike Zrimsek <mikezrimsek@gmail.com>
2016-09-02 20:24:44 -0400

    added ui project new server entry point

---
+++ UI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
