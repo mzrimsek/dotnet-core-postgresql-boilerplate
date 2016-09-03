4106b9ea2b7361ece7b32214632e6a95ae8dab33
Mike Zrimsek <mikezrimsek@gmail.com>
2016-09-02 20:23:52 -0400

    moved anything not core from core

--- Core/Models/View/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Models.View
-{
-    public class UserViewModel
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-    }
-}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,20 +1,12 @@
-using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+using System;
 
-namespace Core
+namespace ConsoleApplication
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseKestrel()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            //required entry point
         }
     }
 }
